75d7f5a66c51ffa6dbeca64973195fbb6826362c
jinoSimg <jinosimg@gmail.com>
2024-02-06 16:55:43 +0900

    4th- Prefabs Commit

---
+++ Assets/Scripts/Pal_Ball.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class Pal_Ball : MonoBehaviour
+{
+   public Player player;
+    public BallState ballState;
+
+    public enum BallState
+    {
+        Catch,
+        Summon
+    }
+    private void Start()
+    {
+    
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (ballState ==BallState.Catch&& collision.gameObject.tag == "Monster")
+        {
+            Debug.Log("monster Captured");
+            player.myPalList.Add(collision.gameObject.GetComponent<Monster>().myId);
+            player.monsterGoList.Add (collision.gameObject);
+            gameObject.SetActive(false);
+           
+        }
+
+        //ToDO: 소환용 팔볼과 포획용 팔볼 버튼은로 조작가능하게 바꾸기
+        if (ballState == BallState.Summon&&collision.gameObject.tag =="Ground")
+        {
+            player.SummonPal();
+            gameObject.SetActive(false);
+            Debug.Log("Pal Spawned");
+        }
+
+    }
+
+   
+}
---
+++ Assets/Scripts/PlayerUI.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+public class PlayerUI : MonoBehaviour
+{
+    public TMP_Text moveModeText;
+    public TMP_Text BallModeText;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
